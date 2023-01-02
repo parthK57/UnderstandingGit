475fd36c15ec72e4c0a22fef437ead4c981a57a8
parthK57 <parth.kolgiri32@gmail.com>
2023-01-02 11:52:57 +0530

    Assignment 3 added

---
+++ DOM Manipulations/Assignment - 3/dom.js
@@ -0,0 +1,6 @@
+const title = document.querySelector("#main-header");
+const addItem = document.querySelector(".title");
+
+addItem.style.fontWeight = "bold";
+addItem.style.color = "green";
+title.style.borderBottom = "3px solid black";
