87340440e1072f4d3759d004c175f361f288a5fd
parthK57 <parth.kolgiri32@gmail.com>
2023-01-02 16:41:18 +0530

    Edited the li tag with querySelector & querySelectorAll

--- DOM Manipulations/Assignment - 3/dom.js
+++ DOM Manipulations/Assignment - 3/dom.js
@@ -2,8 +2,15 @@ const title = document.querySelector("#main-header");
 const addItem = document.querySelector(".title");
 
 title.style.borderBottom = "3px solid black";
-const liClassName = document.getElementsByClassName("new-list-group-item"); 
-const li = document.getElementsByTagName("li");
+const li2 = document.querySelector(".list-group-item:nth-child(2)");
+const li3 = document.querySelector(".list-group-item:nth-child(3)");
 
-liClassName[0].style.fontWeight = "100";
-li[4].style.color = "yellow";
+li2.style.background = "green";
+//li3.style.display = "none";
+
+const qli = document.querySelectorAll(".list-group-item");
+qli[1].style.color = "yellow";
+
+for(let i = 0; i < qli.length; i++){
+    if(i%2 != 0) qli[i].style.background = "green";
+}
