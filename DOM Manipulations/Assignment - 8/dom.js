0e830298b184fda43daaf9934603c1b3f7b48e3f
parthK57 <parth.kolgiri32@gmail.com>
2023-01-03 13:21:58 +0530

    Assignmet 9 added

--- DOM Manipulations/Assignment - 8/dom.js
+++ DOM Manipulations/Assignment - 8/dom.js
@@ -1,12 +1,6 @@
 const form = document.getElementById("addForm");
-let itemList = document.querySelector("#items");
-let deleteBtnG = document.querySelectorAll(
-  "button[class='btn btn-danger btn-sm float-right delete']"
-);
-console.log("Item List-");
-console.log(itemList);
-console.log("Delete Button List");
-console.log(deleteBtnG);
+const itemList = document.querySelector("#items");
+var filter = document.getElementById("filter");
 
 // submit event
 const addItem = (e) => {
@@ -22,25 +16,42 @@ const addItem = (e) => {
   deleteBtn.appendChild(document.createTextNode("X"));
   newLi.appendChild(deleteBtn);
   itemList.appendChild(newLi);
-  itemList = document.querySelector("#items");
-  deleteBtnG = document.querySelectorAll(
-    "button[class='btn btn-danger btn-sm float-right delete']"
-  );
-  console.log("Item List-");
-  console.log(itemList);
-  console.log("Delete Button List");
-  console.log(deleteBtnG);
+
+  const editBtn = document.createElement("button");
+  editBtn.className = "btn btn-warning btn-sm float-right warning";
+  editBtn.appendChild(document.createTextNode("X"));
+  newLi.appendChild(editBtn);
+  itemList.appendChild(newLi);
 };
+
+form.addEventListener("submit", addItem);
 
 // delete event
 const removeItem = (e) => {
-  e.preventDefault();
-  const parentElement = e.target.parentElement;
-  itemList.removeChild(parentElement);
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are You Sure?")) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
 };
 
-for (let i = 0; i < deleteBtnG.length; i++) {
-  deleteBtnG[i].addEventListener("click", removeItem);
-}
+itemList.addEventListener("click", removeItem);
 
-form.addEventListener("submit", addItem);
+// filter event
+
+filter.addEventListener("keyup", filterItems);
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+
+    const abc = Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
