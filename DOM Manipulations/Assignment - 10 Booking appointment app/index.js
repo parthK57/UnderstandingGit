5b312acec22f5a195a6cdfb296d95bcff495404e
parthK57 <parth.kolgiri32@gmail.com>
2023-01-04 14:57:37 +0530

    Assignment 14 added - edit functionality with all the bugs solved

--- DOM Manipulations/Assignment - 10 Booking appointment app/index.js
+++ DOM Manipulations/Assignment - 10 Booking appointment app/index.js
@@ -10,8 +10,8 @@ if (count == null) {
   localStorage.setItem("count", "0");
 }
 
+// adding new user
 submitBtn.addEventListener("click", addUser);
-
 function addUser(e) {
   e.preventDefault();
   count = parseInt(localStorage.getItem("count"));
@@ -23,6 +23,7 @@ function addUser(e) {
   email.value = "";
 }
 
+// display previous data
 function display() {
   for (let i = 0; i < count; i++) {
     const userDetails = localStorage.getItem(`${i}`);
@@ -43,7 +44,7 @@ function display() {
       deleteBtn.className = "delete-btn";
       newDiv.className = "display-users";
       deleteBtn.innerHTML = `Delete`;
-      const text = `Name:${userName} <span></span> Email:${userEmail}`;
+      const text = `Name:${userName}<span></span>Email:${userEmail}`;
       newDiv.innerHTML = text;
       btnContainer.appendChild(updateBtn);
       btnContainer.appendChild(deleteBtn);
@@ -56,6 +57,7 @@ function display() {
 }
 display();
 
+// update display
 function updateDisplay(name, email) {
   // document creation
   const formContainerDiv = document.querySelector(".form-container");
@@ -68,7 +70,7 @@ function updateDisplay(name, email) {
   const updateBtn = document.createElement("button");
   updateBtn.className = "update-btn";
   updateBtn.innerHTML = "Update";
-  const text = `Name:${name.value} <span></span> Email:${email.value}`;
+  const text = `Name:${name.value}<span></span>Email:${email.value}`;
   newDiv.innerHTML = text;
   newDiv.className = "display-users";
   btnContainer.appendChild(updateBtn);
@@ -77,70 +79,192 @@ function updateDisplay(name, email) {
   formContainerDiv.appendChild(newDiv);
   deleteBtnG = document.querySelectorAll(".delete-btn");
   updateUserG = document.querySelectorAll(".update-btn");
+
+  // delete button logic
   for (let i = 0; i < deleteBtnG.length; i++) {
     deleteBtnG[i].addEventListener("click", deleteUser);
     function deleteUser() {
       const parentElement = deleteBtnG[i].parentElement.parentElement;
-      parentElement.remove();
-      localStorage.removeItem(`${i}`);
+      const rawUserName = parentElement.firstChild.textContent;
+      const rawUserEmail =
+        parentElement.firstChild.nextSibling.nextSibling.textContent;
+      const parsedUserName = rawUserName.split(":")[1];
+      const parsedUserEmail = rawUserEmail.split(":")[1];
+      const value = parsedUserName + "-" + parsedUserEmail;
+
+      // Algorithm
+      let flag = true;
+      let iterator = 0;
+      while (flag) {
+        const iteratorValue = localStorage.getItem(`${iterator}`);
+        if (iteratorValue == value) {
+          parentElement.remove();
+          localStorage.removeItem(`${iterator}`);
+          flag = false;
+        }
+        iterator++;
+      }
     }
   }
+
+  // update button logic
+  for (let i = 0; i < updateUserG.length; i++) {
+    updateUserG[i].addEventListener("click", updateUser);
+    // bigPapa = actual div containing the user details
+    const bigPapa = updateUserG[i].parentElement.parentElement;
+    // const olderUserName = updateUserG[i].parentElement.parentElement.
+    function updateUser() {
+      const modal = document.createElement("div");
+      const modalTitle = document.createElement("div");
+      modalTitle.className = "modal-title";
+      modalTitle.innerText = "Update User";
+  
+      // Name label & input
+      const nameLabel = document.createElement("label");
+      const nameInput = document.createElement("input");
+      nameLabel.className = "modal-label";
+      nameLabel.innerHTML = "Name:";
+  
+      // Email label
+      const emailLabel = document.createElement("label");
+      const emailInput = document.createElement("input");
+      emailLabel.className = "modal-label";
+      emailLabel.innerHTML = "Email:";
+  
+      // Submit button
+      const submitBtn = document.createElement("button");
+      submitBtn.className = "submit-btn";
+      submitBtn.innerHTML = "Submit";
+      submitBtn.addEventListener("click", closeModal);
+      function closeModal() {
+        const rawUserName = bigPapa.firstChild.textContent;
+        const rawUserEmail = bigPapa.firstChild.nextSibling.nextSibling.textContent;
+        const userName = rawUserName.split(":")[1];
+        const userEmail = rawUserEmail.split(":")[1];
+        const value = userName+"-"+userEmail;
+  
+        // Local Storage Logic
+        let iterator = 0;
+        let flag = true;
+        while(flag){
+          if(localStorage.getItem(`${iterator}`) == value){
+            localStorage.setItem(`${iterator}`, `${nameInput.value}-${emailInput.value}`);
+            flag = false;
+          }
+          iterator++;
+        }
+        bigPapa.remove();
+        modal.remove();
+        updateDisplay(nameInput, emailInput);
+        emailInput.value = "";
+        nameInput.value = "";
+      }
+  
+      // Lexicographical addition of elements
+      modal.appendChild(modalTitle);
+      modal.appendChild(nameLabel);
+      modal.appendChild(nameInput);
+      modal.appendChild(emailLabel);
+      modal.appendChild(emailInput);
+      modal.appendChild(submitBtn);
+      modal.className = "modal";
+      const body = document.querySelector("body");
+      body.appendChild(modal);
+    }
+  }  
 }
 
 // deleting an user
 deleteBtnG = document.querySelectorAll(".delete-btn");
-
 for (let i = 0; i < deleteBtnG.length; i++) {
   deleteBtnG[i].addEventListener("click", deleteUser);
   function deleteUser() {
     const parentElement = deleteBtnG[i].parentElement.parentElement;
-    parentElement.remove();
-    localStorage.removeItem(`${i}`);
+    const rawUserName = parentElement.firstChild.textContent;
+    const rawUserEmail =
+      parentElement.firstChild.nextSibling.nextSibling.textContent;
+    const parsedUserName = rawUserName.split(":")[1];
+    const parsedUserEmail = rawUserEmail.split(":")[1];
+    const value = parsedUserName + "-" + parsedUserEmail;
+
+    // Algorithm
+    let flag = true;
+    let iterator = 0;
+    while (flag) {
+      const iteratorValue = localStorage.getItem(`${iterator}`);
+      if (iteratorValue == value) {
+        parentElement.remove();
+        localStorage.removeItem(`${iterator}`);
+        flag = false;
+      }
+      iterator++;
+    }
   }
 }
 
 // update an user
 updateUserG = document.querySelectorAll(".update-btn");
+for (let i = 0; i < updateUserG.length; i++) {
+  updateUserG[i].addEventListener("click", updateUser);
+  // bigPapa = actual div containing the user details
+  const bigPapa = updateUserG[i].parentElement.parentElement;
+  // const olderUserName = updateUserG[i].parentElement.parentElement.
+  function updateUser() {
+    const modal = document.createElement("div");
+    const modalTitle = document.createElement("div");
+    modalTitle.className = "modal-title";
+    modalTitle.innerText = "Update User";
 
-// for (let i = 0; i < updateUserG.length; i++) {
-//   updateUserG[i].addEventListener("click", updateUser);
-//   const bigPapa = updateUserG[i].parentElement.parentElement;
-//   // const olderUserName = updateUserG[i].parentElement.parentElement.
-//   function updateUser() {
-//     const modal = document.createElement("div");
+    // Name label & input
+    const nameLabel = document.createElement("label");
+    const nameInput = document.createElement("input");
+    nameLabel.className = "modal-label";
+    nameLabel.innerHTML = "Name:";
 
-//     // Name label & input
-//     const nameLabel = document.createElement("label");
-//     const nameInput = document.createElement("input");
-//     nameLabel.className = "modal-label";
-//     nameLabel.innerHTML = "Name:";
+    // Email label
+    const emailLabel = document.createElement("label");
+    const emailInput = document.createElement("input");
+    emailLabel.className = "modal-label";
+    emailLabel.innerHTML = "Email:";
 
-//     // Email label
-//     const emailLabel = document.createElement("label");
-//     const emailInput = document.createElement("input");
-//     emailLabel.className = "modal-label";
-//     emailLabel.innerHTML = "Email:";
+    // Submit button
+    const submitBtn = document.createElement("button");
+    submitBtn.className = "submit-btn";
+    submitBtn.innerHTML = "Submit";
+    submitBtn.addEventListener("click", closeModal);
+    function closeModal() {
+      const rawUserName = bigPapa.firstChild.textContent;
+      const rawUserEmail = bigPapa.firstChild.nextSibling.nextSibling.textContent;
+      const userName = rawUserName.split(":")[1];
+      const userEmail = rawUserEmail.split(":")[1];
+      const value = userName+"-"+userEmail;
 
-//     // Submit button
-//     const submitBtn = document.createElement("button");
-//     submitBtn.className = "submit-btn";
-//     submitBtn.innerHTML = "Submit";
-//     submitBtn.addEventListener("click", closeModal);
-//     function closeModal() {
-//       // Local Storage Logic
-//       localStorage.setItem(`${i}`, `${nameInput.value}-${emailInput.value}`);
-//       bigPapa.remove();
-//       modal.remove();
-//     }
+      // Local Storage Logic
+      let iterator = 0;
+      let flag = true;
+      while(flag){
+        if(localStorage.getItem(`${iterator}`) == value){
+          localStorage.setItem(`${iterator}`, `${nameInput.value}-${emailInput.value}`);
+          flag = false;
+        }
+        iterator++;
+      }
+      bigPapa.remove();
+      modal.remove();
+      updateDisplay(nameInput, emailInput);
+      emailInput.value = "";
+      nameInput.value = "";
+    }
 
-//     // Lexicographical addition of elements
-//     modal.appendChild(nameLabel);
-//     modal.appendChild(nameInput);
-//     modal.appendChild(emailLabel);
-//     modal.appendChild(emailInput);
-//     modal.appendChild(submitBtn);
-//     modal.className = "modal";
-//     const body = document.querySelector("body");
-//     body.appendChild(modal);
-//   }
-// }
+    // Lexicographical addition of elements
+    modal.appendChild(modalTitle);
+    modal.appendChild(nameLabel);
+    modal.appendChild(nameInput);
+    modal.appendChild(emailLabel);
+    modal.appendChild(emailInput);
+    modal.appendChild(submitBtn);
+    modal.className = "modal";
+    const body = document.querySelector("body");
+    body.appendChild(modal);
+  }
+}
