9bd90e24289a1e6e0f554ef674829e59595fe101
parthK57 <parth.kolgiri32@gmail.com>
2023-01-11 12:21:14 +0530

    promise

---
+++ Dynamic Page/Async Await/index.js
@@ -0,0 +1,8 @@
+console.log("person1: shows the ticket");
+console.log("person2: shows the ticket");
+
+const preMovie = async () => 'premovie';
+preMovie().then((m) => console.log(m));
+
+console.log("person4: shows the ticket");
+console.log("person5: shows the ticket");
